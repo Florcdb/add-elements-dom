092c0af08166fe021d9900437dea86bd16f20c42
Florcdb <florcordoba51@gmail.com>
2023-11-11 15:15:03 +0100

    ultimos cambios

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
  // Aquí tu código
+
+ document.addEventListener("click", function () {
+    const agregar = document.getElementById("agregar");
+    const lista = document.getElementById("lista");
+
+    agregar.addEventListener("click", function () {
+        const edad = prompt("Ingresa tu edad:");
+        const kilos = prompt("Ingresa tu peso en kilogramos:");
+        const diasEntrenamiento = prompt("Ingresa el número de días que entrenas a la semana:");
+
+        if (edad !== null && kilos !== null && diasEntrenamiento !== null) {
+            const mensaje = `Edad: ${edad} años, Peso: ${kilos} kg, Días de entrenamiento: ${diasEntrenamiento} días`;
+
+            const nuevoElemento = document.createElement("li");
+            nuevoElemento.textContent = mensaje;
+
+            lista.appendChild(nuevoElemento);
+        }
+    });
+});
